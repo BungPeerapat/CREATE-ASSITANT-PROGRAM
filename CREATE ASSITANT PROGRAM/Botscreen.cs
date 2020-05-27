5b8e99b2fe3e6cc4d903c11cc15b19299fc6b323
BungPeerapat <mashiro74122541@gmail.com>
2020-05-27 17:40:15 +0700

    Change Folder

---
+++ CREATE ASSITANT PROGRAM/Botscreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CREATE_ASSITANT_PROGRAM
+{
+    public partial class Botscreen : Form
+    {
+        public Botscreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Lime;
+            this.TransparencyKey = Color.Lime;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
---
+++ CREATE ASSITANT PROGRAM/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CREATE_ASSITANT_PROGRAM
+{
+    public partial class LoginMain : Form
+    {
+        public LoginMain()
+        {
+            InitializeComponent();
+        }
+
+        String CodeSend;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Botscreen BS = new Botscreen();
+            BS.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (USERNAMETEXT.Text == "AdminCode" && PASSWORDTEXT.Text == "74122541")
+            {
+                USERNAMETEXT.ForeColor = Color.Green;
+                Console.Beep();
+                PASSWORDTEXT.ForeColor = Color.Green;
+                Console.Beep();
+                MessageBox.Show("Welcome Admin");
+                MainMenu ADMINSIZEOPEN = new MainMenu();
+                CodeSend = USERNAMETEXT.Text;
+                this.Hide();
+
+
+            }
+            if (USERNAMETEXT.Text == "Member" && PASSWORDTEXT.Text == "Pass")
+            {
+                USERNAMETEXT.ForeColor = Color.Green;
+                Console.Beep();
+                PASSWORDTEXT.ForeColor = Color.Green;
+                Console.Beep();
+                MessageBox.Show("Welcome Member");
+                MainMenu MainMenu = new MainMenu();
+                CodeSend = USERNAMETEXT.Text;
+                this.Hide();
+            }
+        }
+
+        private void LoginMain_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Lime;
+            this.TransparencyKey = Color.Lime;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
