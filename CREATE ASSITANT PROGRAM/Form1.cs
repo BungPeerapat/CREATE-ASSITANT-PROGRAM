523deb3411e890158d616ae26e6f07c0080025df
BungPeerapat <mashiro74122541@gmail.com>
2020-06-09 15:16:05 +0700

    Remake Form2

---
+++ CREATE ASSITANT PROGRAM/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CREATE_ASSITANT_PROGRAM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(1042, 0);
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = Color.LimeGreen;
+        }
+    }
+}
